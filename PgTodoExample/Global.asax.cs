3e4f77450eb4c614708ee4244479d1df47c50a63
Carl Hörberg <carl.hoerberg@gmail.com>
2012-10-05 10:38:02 +0200

    touch

--- PgTodoExample/Global.asax.cs
+++ PgTodoExample/Global.asax.cs
@@ -15,6 +15,7 @@ namespace PgTodoExample
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
         }
 
         public static void RegisterRoutes(RouteCollection routes)
